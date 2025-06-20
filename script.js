4e0da897455cf0124590f229aa00bd35bb4d0668
Krupal <krupalthakre2005@gmail.com>
2025-06-20 10:44:55 +0530

    added functionalty for user to make choice for grid by adding a button

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
+function create_grid() {
+    let num = prompt("Enter the dimension of grid");
+    const grid = document.querySelector("#mgrid")
+    if(num>100)
+        num=100;
 
-const grid=document.querySelector("#mgrid")
+    for (let i = 0; i < num; i++) {
+        let temp = document.createElement("div")
+        temp.classList.add("horz");
 
-for(let i=0;i<16;i++){
-    let temp=document.createElement("div")
-    temp.classList.add("horz");
-    
-    for(let j=0;j<16;j++){
-        let box=document.createElement("div")
-        box.classList.add("box");
-        temp.appendChild(box);
+        for (let j = 0; j < num; j++) {
+            let box = document.createElement("div")
+            box.classList.add("box");
+            temp.appendChild(box);
+        }
+        grid.appendChild(temp);
+
     }
-    grid.appendChild(temp);
-   
+    let hover = document.querySelectorAll(".box")
+    hover.forEach(square => {
+        square.addEventListener("mouseover", () => {
+            square.style.backgroundColor = "pink"
+        })
+    })
+    hover.forEach(square => {
+        square.addEventListener("mouseout", () => {
+            square.style.backgroundColor = "white"
+        })
+    })
 }
-let hover=document.querySelectorAll(".box")
-hover.forEach(square=>{
-    square.addEventListener("mouseover",()=>{
-        square.style.backgroundColor="red"
-    })
-})
-hover.forEach(square=>{
-    square.addEventListener("mouseout",()=>{
-        square.style.backgroundColor="white"
-    })
-})
+const but = document.querySelector(".choice")
+but.addEventListener("click",create_grid)
+
